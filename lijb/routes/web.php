12ad2a81c0256e121dc2b622129bfaea7489e052
Slawgorodskij <Slawgorodskij@mail.ru>
2022-08-24 00:00:55 +0700

    Homework lesson 2

--- lijb/routes/web.php
+++ lijb/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeWorkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/home_work_task_1', [HomeWorkController::class, 'taskOne']);
+Route::get('/home_work_task_2', [HomeWorkController::class, 'taskTwo']);
+Route::get('/home_work_task_3', [HomeWorkController::class, 'taskThree']);
